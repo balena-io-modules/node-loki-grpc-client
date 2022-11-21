b02460898ebf112684ec2af34a501424e51edbe2
Thomas Manning <thomasm@balena.io>
2022-11-21 18:50:12 +1000

    Change grpc to grpc-js
    Rebuild proto js using grpc-js
    Removed `promisifyClient` function, not compatible with `grpc-js` replace with promisify on client methods.
    
    Change-type: major
    Signed-off-by: Thomas Manning <thomasm@balena.io>

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,9 @@
-import * as grpc from 'grpc';
-import { CallOptions, Metadata } from 'grpc';
-import { promisify } from 'util';
+import * as grpc from '@grpc/grpc-js';
 
-export {
-	status,
-	ClientReadableStream,
-	ServiceError,
-	Metadata,
-	CallOptions,
-} from 'grpc';
+export { status, ClientReadableStream, ServiceError } from '@grpc/grpc-js';
 export * from 'google-protobuf/google/protobuf/timestamp_pb.js';
 export * from '../proto/compiled/logproto_grpc_pb';
 export * from '../proto/compiled/logproto_pb';
-
-export interface IGrpcClientAsync {
-	[key: string]: IGrpcMethod;
-}
-
-export type IGrpcMethod = (
-	data: any,
-	metadata?: Metadata,
-	options?: CallOptions,
-) => Promise<any>;
-
-export function promisifyClient(client: any): IGrpcClientAsync {
-	const promisifiedClient = {} as IGrpcClientAsync;
-	for (const name of Object.values<string>(client.$method_names)) {
-		promisifiedClient[name] = promisify(client[name].bind(client));
-	}
-	return promisifiedClient;
-}
 
 export function createInsecureCredentials() {
 	return grpc.credentials.createInsecure();
